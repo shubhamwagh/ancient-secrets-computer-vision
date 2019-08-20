447d2cdf53d75e312e11818b986b35c4ad31d928
Shubham Wagh <shubham.wagh@q-bot.co>
2019-08-20 16:26:21 +0100

    Completed vision-hw4 python script

--- vision-hw4/tryml.py
+++ vision-hw4/tryml.py
@@ -6,9 +6,14 @@ def softmax_model(inputs, outputs):
 
 def neural_net(inputs, outputs):
     print inputs
-    l = [   make_layer(inputs, 32, LOGISTIC),
+    l = [   make_layer(inputs, 64, RELU),
+            make_layer(64, 32, RELU),
             make_layer(32, outputs, SOFTMAX)]
     return make_model(l)
+
+
+'''
+MNIST Dataset
 
 print("loading data...")
 train = load_classification_data("mnist.train", "mnist.labels", 1)
@@ -18,12 +23,13 @@ print
 
 print("training model...")
 batch = 128
-iters = 1000
-rate = .01
+iters = 3000
+rate = 0.1
 momentum = .9
-decay = .0
+decay = 0.01
 
-m = softmax_model(train.X.cols, train.y.cols)
+#m = softmax_model(train.X.cols, train.y.cols)
+m = neural_net(train.X.cols, train.y.cols)
 train_model(m, train, batch, iters, rate, momentum, decay)
 print("done")
 print
@@ -31,6 +37,63 @@ print
 print("evaluating model...")
 print("training accuracy: %f", accuracy_model(m, train))
 print("test accuracy:     %f", accuracy_model(m, test))
+
+'''
+
+# CIFAR dataset
+print("loading data...")
+train = load_classification_data("cifar.train", "cifar/labels.txt", 1)
+test  = load_classification_data("cifar.test", "cifar/labels.txt", 1)
+print("done")
+print
+
+print("training model...")
+batch = 128
+iters = 1000
+rate = 0.01
+momentum = .9
+decay = 0.01
+
+#m = softmax_model(train.X.cols, train.y.cols)
+m = neural_net(train.X.cols, train.y.cols)
+train_model(m, train, batch, iters, rate, momentum, decay)
+print("done")
+print
+
+print("evaluating model...")
+print("training accuracy: %f", accuracy_model(m, train))
+print("test accuracy:     %f", accuracy_model(m, test))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ## Questions ##
